fa047389d131a70cb610a4fd54cfbf019e2fa2ec
Anatol Bir <you@example.com>
2022-09-29 21:48:30 +0800

    Добавила изменение

--- hw_1/Program.cs
+++ hw_1/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
 
+num = Math.Abs(num);
+
 if (num < 100 || num > 999){
 Console.WriteLine("Некорректное число");
 } else {
