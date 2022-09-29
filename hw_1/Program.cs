86a5f9c29cc9fb567618478cb06844763c6a1ec7
Anatol Bir <you@example.com>
2022-09-29 21:30:22 +0800

    Ex_1

---
+++ hw_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num > 999){
+Console.WriteLine("Некорректное число");
+} else {
+    Console.WriteLine(num.ToString()[1]);
+}
+
