c8dd9007da23a165a88dc702e41b94b2836c0935
Anatol Bir <you@example.com>
2022-09-29 22:23:43 +0800

    hw_2

---
+++ hw_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    Console.WriteLine(num);
+    Console.WriteLine("Третья цифра " + num % 10);
+}
+
