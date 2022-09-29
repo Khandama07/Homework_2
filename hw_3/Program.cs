af23d485f42f8eaa916891babca24d7b87352db0
Anatol Bir <you@example.com>
2022-09-29 22:48:13 +0800

    hw_3

---
+++ hw_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+int day = Convert.ToInt32(Console.ReadLine());
+day = Math.Abs(day);
+
+if (day > 0 && day < 8)
+{
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine ("Выходной день");
+    }
+else 
+{Console.WriteLine ("Будний день");
+}
+}
